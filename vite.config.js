1d95b60f681465e45bd3325526acccd963ed1476
Giridhar <giridharkoundinya436@gmail.com>
2025-11-16 16:34:28 +0530

    add vite.config.js rollup fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    target: "esnext",
+    modulePreload: false,
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    force: true,
   },
 });
+
