63a70de2521b1f1390eede819e7e3d6c8ab5aee3
Giridhar <giridharkoundinya436@gmail.com>
2025-11-24 15:05:27 +0530

    Wrap App component with ClubsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext.jsx';
+import { ClubsProvider } from './contexts/ClubsContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <ClubsProvider>
+        <App />
+      </ClubsProvider>
     </AuthProvider>
   </StrictMode>,
 );
